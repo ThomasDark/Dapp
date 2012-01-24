b60562e6fc00f3fa2719da57358f684dfbb6d2be
Thomas Dark <tom@vaultrms.com>
2012-01-24 10:32:15 -0700

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dapp::Application.config.secret_token = '3bb6fccd201826c56a136235dcc9a18ecb6fea4ef28e22f45fe6252b91a6a9383c4071a07e2a4ce06756582f4cfc8d6988ef483dd4e0d51f9392227649730bb9'
